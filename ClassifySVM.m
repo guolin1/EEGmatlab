f4fc1f1fbf927cbc5ebe6dc1c4f50d2f2d8a7271
guolin1 <lawrence.guo91@gmail.com>
2019-12-17 18:08:05 -0500

    Added flexibility
    Added feature pre-processing options, SVM option (libsvm vs. liblinear) and permutation option.

--- ClassifySVM.m
+++ ClassifySVM.m
@@ -1,6 +1,6 @@
-function [accuracies] = ClassifySVM(data1, data2, option)
+function [accuracies] = ClassifySVM(data1, data2, preprocessoption, svmoption, permoption)
 %% Description
-% [accuracies] = ClassifySVM(data1,data2,option);
+% [accuracies] = ClassifySVM(data1, data2, preprocessoption, svmoption, permoption);
 % Required package: LIBSVM [https://www.csie.ntu.edu.tw/~cjlin/libsvm/]
 %
 % Takes two data arrays, one for training & one for testing (cross-decode is
@@ -11,18 +11,26 @@ function [accuracies] = ClassifySVM(data1, data2, option)
 % 
 % ClassifySVM requires libsvm toolbox
 %
-% Liblinear
-% if option = 1, liblinear is used intead of libsvm 
+% 
+% preprocessoption [standardize(1/0), windsorize(1/0), normalize(1/0)]
+%             a vector of 3 values to enable / disable standardization,
+%               windsorization, and normalization. Standardization and
+%               normalization of both training and testing sets are
+%               computed using the parameters (mean, std, max and min) of 
+%               the training set. 
+%               Default: [1 1 1];
+%
+% if svmoption = 1, liblinear is used intead of libsvm 
 %			  liblinear apparently works better when number of feature is
 %			  extremely large. By default, ClassifySVM uses libSVM.
+%               Default: [0]
 %
-% ClassifySVM automatically standardizes and normalizes data separately at
-%       each feature. standardization and normalization are done using the 
-%       means & stds obtained from the training data, and the same means 
-%       and stds are used to standardize the testing data. 
+% if permoption = 1, permutation is enabled and classification is based on
+%               shuffled labels. 
+%               Default: [0]
 %
-% data requirement [IMPORTANT]: 
-% data structure:	[rows] Observations x  [columns] numFeatures
+% data format [IMPORTANT]: 
+% data structure:	[rows] Observations x  [columns] Features
 % 				top half rows = category 1; bottom half rows = category 2  
 % 				number of observations across categories must be equal
 %				Optional: time can be included as the third dimension. 
@@ -32,13 +40,20 @@ function [accuracies] = ClassifySVM(data1, data2, option)
 
 %% check input
 if nargin < 3
-    option = 0; %libsvm
+    svmoption = 0; %libsvm
+    preprocessoption = [1 1 1];
+    permoption = 0;
 end
 
 nFold = size(data1,1)/2;
 %% create cross validation & group labels
 cvlabels = [1:nFold,1:nFold];
 grouplabels = [ones(size(data1, 1)/2,1); ones(size(data1, 1)/2,1).*2];
+% permute grouplabels if permutation is enabled.
+if permoption
+    rng('shuffle');
+    grouplabels = grouplabels(randperm(length(grouplabels)));
+end
 
 %% loop through nTimes
 nTimes = size(data1,3);
@@ -49,27 +64,34 @@ for itime = 1:nTimes
     for icv = 1:nFold
         trainfold = traindata(cvlabels~=icv, :);
         testfold = testdata(cvlabels==icv, :);
-        % normalize trainfold & testfold
-		% Compute mean & standard deviation from the training data [at each feature]
-        meanvec = nanmean(trainfold); stdvec = nanstd(trainfold); 
-		% Standardizes testing data & training data using the same mean & std. Also threshold all data at +/- 3 
-		% to curb the impact of outliers on SVM performance
-        trainfold = (trainfold-meanvec)./stdvec; trainfold(trainfold>3) = 3; trainfold(trainfold<-3) = -3;
-        testfold = (testfold-meanvec)./stdvec; testfold(testfold>3) = 3; testfold(testfold<-3) = -3;
-		% Normalizes between 0 and 1 according to the training data
-        trainfold = (trainfold-min(trainfold))./(max(trainfold)-min(trainfold));
-        testfold = (testfold-min(trainfold))./(max(trainfold)-min(trainfold));
-        
+		% Feature preprocessing
+        if preprocessoption(1)==1 % standardization
+            % Compute mean & standard deviation from the training data [at each feature separately]
+            meanvec = nanmean(trainfold); stdvec = nanstd(trainfold);
+            % Standardizes testing data & training data using the same mean & std.
+            trainfold = (trainfold-meanvec)./stdvec; trainfold(trainfold>3) = 3;
+            testfold = (testfold-meanvec)./stdvec; testfold(testfold>3) = 3;
+        end
+        if preprocessoption(2) == 1 % windsorization
+            %threshold all data at +/- 3 to curb the impact of outliers on SVM performance
+            trainfold(trainfold<-3) = -3;
+            testfold(testfold<-3) = -3;
+        end
+        if preprocessoption(3) == 1 % normalization
+            % Normalizes between 0 and 1 according to the training data
+            trainfold = (trainfold-min(trainfold))./(max(trainfold)-min(trainfold));
+            testfold = (testfold-min(trainfold))./(max(trainfold)-min(trainfold));
+        end
 		% Labels are automatically computed given that data structure follows the requirement stated under description.
         trainlabel = grouplabels(cvlabels~=icv);
         testlabel = grouplabels(cvlabels==icv);
-        if option %liblinear
+        if svmoption %liblinear
             disp(icv);
             model = train(trainlabel, sparse(trainfold), '-q -c 1');
             [predictedlabels, ~, ~] = predict(testlabel, sparse(testfold), model, '-q');
         else
             model = svmtrain(trainlabel, double(trainfold), sprintf('-q -t 0 -c %f', 1));
-            [predictedlabels, ~, decVal] = svmpredict(testlabel, double(testfold), model, '-q');
+            [predictedlabels, ~, ~] = svmpredict(testlabel, double(testfold), model, '-q'); %third output from svmpredict is decVal, used for calculation of DV (see commented out section below).
             % w = model.SVs' * model.sv_coef; % compute weight vector
             % DV = abs(decVal)./norm(w); % decision value is the distance between test data & decision boundary
         end
