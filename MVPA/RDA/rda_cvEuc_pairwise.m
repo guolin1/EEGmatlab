c4d31604e1e6e616edc635d79f9da493a84f98c1
guolin1 <lawrence.guo91@gmail.com>
2019-12-18 11:42:48 -0500

    Create rda_cvEuc_pairwise.m

---
+++ MVPA/RDA/rda_cvEuc_pairwise.m
@@ -0,0 +1,50 @@
+function eucDistance = cvEuc_pairwise(iidata,jjdata,nConds)
+%% Description
+% RSAClassify(data,nConds)
+% Data is a matrix of data for a specific timepoint, nConds = number of
+% conditions
+if nargin<3
+    nConds = jjdata;
+    for iCond = 1:nConds
+        for jCond = iCond:nConds
+            iData = squeeze(iidata(iCond,:,:));
+            jData = squeeze(iidata(jCond,:,:));
+            nFold = size(iData,1);
+            dists = zeros(nFold,nFold);
+            for icv = 1:nFold-1
+                for jcv = icv+1:nFold
+                    dist_train = iData(icv, :) - jData(icv,:);
+                    dist_test = iData(jcv, :) - jData(jcv,:);
+                    % cross validate on different folds
+                    dists(icv,jcv) = dot(dist_train,dist_test);
+                end
+            end
+            dists = (dists + dists');
+            eucDistance(iCond,jCond) = sum(dists(:))./(nFold*nFold-nFold);
+        end
+    end
+else
+    for iCond = 1:nConds
+        % needs to be 1:nConds rather than iCond:nConds because these are 
+        % different data (i.e., iidata vs jjdata), and so unlike the RSA 
+        % above, here, iidata(iCond==2) vs jjdata(iCond==3) is not the same 
+        % as iidata(iCond==3) vs jjdata(iCond==2). 
+        for jCond = 1:nConds 
+            iData = squeeze(iidata(iCond,:,:));
+            jData = squeeze(jjdata(jCond,:,:));
+            
+            nFold = size(iData,1);
+            dists = zeros(nFold,nFold);
+            for icv = 1:nFold
+                for jcv = 1:nFold
+                   dist_train = iData(icv,:) - jData(icv,:);
+                   dist_test = iData(jcv,:) - jData(jcv,:);
+                   % cross validate on different folds
+                   dists(icv,jcv) = dot(dist_train,dist_test);
+                end
+            end
+            eucDistance(iCond,jCond) = nanmean(dists(eye(nFold,nFold)==0));
+        end
+    end
+end
+end
