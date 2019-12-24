1dc9842607220ce753c983812943d72f97a5c0e1
Lin Lawrence Guo <lawrence.guo91@gmail.com>
2019-12-24 10:47:12 -0500

    Update preprocess_mnn.m

--- MVPA/RDA/Preprocess/preprocess_mnn.m
+++ MVPA/RDA/Preprocess/preprocess_mnn.m
@@ -1,6 +1,38 @@
-function output = preprocess_mnn(input)
+function mnn_Data = preprocess_mnn(Data)
 %% Description
-% performs multivariate noise normalisation
+% mnn_Data = preprocess_mnn(Data)
+% conducts multivariate noise normalisation on data following Guggenmos et
+% al (2018) Multivariate pattern analysis for MEG: a comparison of
+% dissimilarity measures. NeuroImage 173
+%
+% input:
+%       Data: nConditions, nChannels, nTimes, nObservations
+%
+% output:
+%       mnn_Data: nConditions, nChannels, nTimes, nObservations
+%
+% IMPORTANT: Data input must include all conditions in order for mnn to be
+%            non-biased. 
+%
+% Lawrence December 2019
+[nConds, nElecs, nTimes, nObs] = size(Data);
+Data = permute(Data,[1 4 2 3]);
 
-
+disp('Obtaining covariance matrices from ERP for mnn');
+% Select epoch time points
+for iCond = 1:nConds
+    for iTime = 1:nTimes
+        icov(iCond,iTime,:,:) = covCor(squeeze(Data(iCond,:,:,iTime))); % calculate covariance matrix at each time point
+    end
+end
+sig = squeeze(nanmean(nanmean(icov1,2),1)); % average covariance matrices across time, and then across conditions
+disp('Normalizing by covariance matrix');
+% concatenate condition & trials to make normalisation faster
+Data = reshape(Data, [nConds*nObs, nElecs, nTimes]);
+parfor iTime = 1:nTimes
+    idata = squeeze(Data(:,:,iTime));
+    ndata(:,:,iTime) = idata * (sig^(-1/2)); % normalize SOA1 data
+end
+mnn_Data = reshape(ndata,[nConds,nObs,nElecs,nTimes]);
+mnn_Data = permute(mnn_Data, [1 3 4 2]);
 end
